dc9ff53faf1e770a1c99879a2b79ebb28e782e9c
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-12-19 15:33:34 +0530

    chnages

---
+++ Day 2/Day 2 Assignments/ConditionalAssignments/LargestOfThree.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ConditionalAssignments;
+
+public class LargestOfThree
+{
+
+}
--- Day 2/Day 2 Assignments/ConditionalAssignments/Program.cs
+++ Day 2/Day 2 Assignments/ConditionalAssignments/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConditionalAssignments;
+
 Console.WriteLine("Hello, World!");
+
+HeightChecker.HeightMain();
