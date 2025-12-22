5b03b859d3e1bcc541eb39dd4e6f9de47352e4a0
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-12-22 20:54:13 +0530

    First Assignment start and implementing some functionalities.

---
+++ Day 4/Assignments/Assessment1/Employee.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Assessment1;
+
+public class Employee
+{
+    int age;
+    int depChoice;
+
+    public int EmpID{get; set;}
+
+    public string Name {get; set;}
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            if (age >= 21)
+            {
+                age = value;
+            }
+            else
+            {
+                throw new Exception("The Age is below 21");
+            }
+        }
+    }
+    public int DepChoice
+    {
+        get
+        {
+            return depChoice;
+        }
+        set
+        {
+            if(value == 1 || value == 2 || value == 3)
+            {
+                depChoice = value;
+
+            }
+            else
+            {
+                throw new Exception("The value should be 1 or 2 or 3 Nothing else");
+            }
+        }
+    }
+    public double BasSal{get; set;}
+
+
+    
+
+
+
+
+
+}
