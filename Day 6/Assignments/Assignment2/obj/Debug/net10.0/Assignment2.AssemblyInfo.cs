015c243b5bbe994982af4496b23262e818042db5
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-12-24 15:55:24 +0530

    Second Build

--- Day 6/Assignments/Assignment2/obj/Debug/net10.0/Assignment2.AssemblyInfo.cs
+++ Day 6/Assignments/Assignment2/obj/Debug/net10.0/Assignment2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6e0b607d90630c97eb8eb35407bf7960ee6265")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc3ba66624fd76babded4ff982e5c72a690dec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
