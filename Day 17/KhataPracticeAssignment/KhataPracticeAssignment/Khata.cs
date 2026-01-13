04f518ffc4c5a7d87f9a50d6ff56b6fd578359ad
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-13 14:56:57 +0530

    Practice Assignments Khata Book

---
+++ Day 17/KhataPracticeAssignment/KhataPracticeAssignment/Khata.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KhataPracticeAssignment
+{
+    public class Khata
+    {
+        public Dictionary<string, int> products = new Dictionary<string, int>();
+
+
+        public Khata(Dictionary<string, int> record)
+        {
+            products = record;
+        }
+
+        public int getTotal()
+        {
+            int sum = 0;
+            foreach (var item in products)
+            {
+                sum += item.Value;
+            }
+
+            return sum;
+        }
+
+
+
+        /// <summary>
+        ///     getRepeatAmount
+        /// 	Returns the number of unique amounts that occur more than once.
+        /// 	Each repeated amount should be counted only once.
+
+        /// </summary>
+        /// <returns></returns>
+
+        public int getRepeatAmount()
+        {
+            int repeatedAmountCount = 0;
+            HashSet<int> set = new HashSet<int>();
+            foreach(var item in products)
+            {
+                if (set.Contains(item.Value)) {
+                    repeatedAmountCount++;
+                }
+                else
+                {
+                    set.Add(item.Value);
+                }
+            }
+
+            return repeatedAmountCount;
+        }
+
+
+        public void AddItem(string Name, int amount)
+        {
+            if (products.ContainsKey(Name))
+            {
+                Console.WriteLine("Product is already Added");
+            }
+            else
+            {
+                products.Add(Name, amount);
+            }
+        }
+
+
+
+    }
+ 
+}
---
+++ Day 17/KhataPracticeAssignment/KhataPracticeAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KhataPracticeAssignment;
+
+Console.WriteLine("Welcome to Khata Ebook");
+
+
+Console.WriteLine("Press 1 to add Item and amount. \n Any other Key to exit");
+int choice = Convert.ToInt32(Console.ReadLine());
+Dictionary<string,int> khataInfo = new Dictionary<string,int>();
+
+Khata khata = new Khata(khataInfo);
+
+while(choice == 1)
+{
+    Console.WriteLine("Enter the name of the Item: ");
+    string ?name = Console.ReadLine();
+    Console.WriteLine("Enter the Price of the Item: ");
+    int price = Convert.ToInt32(Console.ReadLine());
+    khata.AddItem(name, price);
+    Console.WriteLine("Press 1 to Add More Item. \n Any other key to exit");
+    choice = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Total Amount: "+ khata.getTotal());
+Console.WriteLine("Repeated Amount Count: "+ khata.getRepeatAmount());
+
+
