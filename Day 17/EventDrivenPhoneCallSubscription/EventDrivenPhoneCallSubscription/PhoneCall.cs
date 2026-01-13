7ce105a706b5aaf54f0b63b46a7aacedf58fc9c0
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-13 15:36:10 +0530

    EventDrivenPhoneCallSubscription Assignmnet

---
+++ Day 17/EventDrivenPhoneCallSubscription/EventDrivenPhoneCallSubscription/PhoneCall.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventDrivenPhoneCallSubscription
+{
+
+    public delegate void Notify();
+    public class PhoneCall
+    {
+        public string? Message { get; private set;  }
+        public event Notify PhoneCallevent;
+        
+
+        private void OnSubscribe()
+        {
+            Message = "Subscribed to call";
+        }
+
+        private void OnUnsubscribe()
+        {
+            Message = "UnSubscribed to call";
+        }
+
+        public void MakeAPhoneCall(bool notify)
+        {
+            //Console.WriteLine("Make a phone Call Called");
+            if (notify)
+            {
+                PhoneCallevent = OnSubscribe;
+            }
+            else
+            {
+                PhoneCallevent = OnUnsubscribe;
+            }
+
+            PhoneCallevent();
+
+        }
+
+        public void DisplayMessage()
+        {
+            //Console.WriteLine("Display MEssage CAlled");
+            Console.WriteLine(Message);
+        }
+    }
+
+    
+}
---
+++ Day 17/EventDrivenPhoneCallSubscription/EventDrivenPhoneCallSubscription/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EventDrivenPhoneCallSubscription;
+
+Console.WriteLine("Hello, World!");
+
+
+PhoneCall phoneCall = new PhoneCall();
+phoneCall.MakeAPhoneCall(false);
+Console.WriteLine(phoneCall.Message);
+
