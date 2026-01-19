6d2440bc8c507a05a3b6f86cf96fab4e6fb611dd
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-19 14:55:59 +0530

    q6

---
+++ Top Brains/Q6/Q6/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int largest = 0;
+
+if(a >= b)
+{
+    if (a >= c) largest = a;
+}
+
+if (b >= a)
+{
+    if (b >= c) largest = b;
+}
+
+if (c >= a)
+{
+    if (c >= b) largest = c;
+}
+
+Console.WriteLine("The Largest Number is: "+ largest);
