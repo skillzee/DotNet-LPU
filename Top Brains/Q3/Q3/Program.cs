9bf557c78671b46fd824ee7e53a8d679f9967754
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-19 11:50:34 +0530

    q3

---
+++ Top Brains/Q3/Q3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Write n: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Write upto: ");
+int upto = Convert.ToInt32(Console.ReadLine());
+
+
+for(int i =1; i<= upto; i++)
+{
+    Console.WriteLine(n * i);
+}
+
+
