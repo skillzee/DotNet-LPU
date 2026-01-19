c095f11d71c17871447012083c40fb1b089814c7
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-19 15:18:41 +0530

    q8

---
+++ Top Brains/Q8/Q8/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter the starting digit: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the last digit: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+for(int i = a; i<=b; i++)
+{
+    if (IsLucky(i))
+    {
+        count++; 
+    }
+}
+
+Console.WriteLine("Total numbers are: "+ count);
+
+
+
+bool IsLucky(int i)
+{
+    int secondNum = i * i;
+    int firstNum = i;
+
+    int firstDigitSum = helper(firstNum);
+    int secondDigitSum = helper(secondNum);
+
+    if(firstDigitSum*firstDigitSum ==  secondDigitSum) return true;
+
+    return false;
+}
+
+
+int helper(int num)
+{
+    int sum = 0;
+
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+
+}
