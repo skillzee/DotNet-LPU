ee4a5e6931c0e0dc7c83f5fda2378b18a41ee169
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-19 15:28:50 +0530

    Q9

---
+++ Top Brains/Q9/Q9/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Initial Balance: ");
+int initialBalance = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number of transactions: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] transactions = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    transactions[i] = Convert.ToInt32(Console.ReadLine());
+    if (transactions[i] > 0)
+    {
+        initialBalance += transactions[i];
+    }
+    if (transactions[i] < 0) {
+        if (initialBalance < transactions[i]) continue;
+        initialBalance += transactions[i];
+    }
+}
+
+
+Console.WriteLine("Final Balance "+ initialBalance);
+
+
+
