cc1288c0f7f708dc2f30871d16fdc8ffc927cd2e
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-19 15:04:16 +0530

    q7

---
+++ Top Brains/Q7/Q7/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter the Height");
+int height = Convert.ToInt32(Console.ReadLine());
+if (height < 150)
+{
+    Console.WriteLine("Short");
+}
+else if (height>=150 && height <180)
+{
+    Console.WriteLine("Average");
+}
+else if (height >= 180)
+{
+    Console.WriteLine("Tall");
+}
+
+
