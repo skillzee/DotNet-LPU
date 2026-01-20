cdb7cad6a5ce59fd6e84d5e411b9f0acd46d3aaf
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-20 15:01:38 +0530

    23

---
+++ Top Brains/Q10/Q10/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+object[] arr = new object[n];
+
+for (int i = 0; i < n; i++)
+{
+    arr[i] = Console.ReadLine();
+}
+
+
+int sum = 0;
+
+for(int i = 0; i<n; i++)
+{
+    string ch = (string)arr[i];
+    if (ch.All(Char.IsDigit))
+    {
+        sum += Int32.Parse(ch);
+    }
+}
+
+Console.WriteLine(sum);
