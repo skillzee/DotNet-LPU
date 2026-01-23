9c5cb59c413b71a219813dcf4bab350786cb13c2
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-01-23 16:02:31 +0530

    meeting qquestion

---
+++ Day 20/Linq Task/Linq Task/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq_Task
+{
+    public class Employee
+    {
+        public string Name { get; set;  }
+
+        public int Salary { get; set; }
+    }
+}
---
+++ Day 20/Linq Task/Linq Task/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Linq_Task;
+using System.Xml.Linq;
+
+Console.WriteLine("Hello, World!");
+
+//create an employee class having name and salary.
+//add them to the list and fin the person with highest salary
+
+List<Employee> employess = new List<Employee>()
+{
+    new Employee{ Name = "Parth", Salary = 10000},
+    new Employee {Name = "Raju", Salary = 20000},
+    new Employee {Name = "Shaym", Salary = 30000}
+
+};
+
+
+var personWithHighestSalary = from member in employess
+                              where member.Salary == employess.Max(mem => mem.Salary)
+                              select member;
+
+foreach (var item in personWithHighestSalary)
+{
+    Console.WriteLine(item.Name);
+}
