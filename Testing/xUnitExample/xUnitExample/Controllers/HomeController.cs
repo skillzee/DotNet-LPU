01315c5b57e036d2bbc41f586c92c941d6430c63
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-03-31 00:37:08 +0530

    Testing and moq

---
+++ Testing/xUnitExample/xUnitExample/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xUnitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        public string Index()
+        {
+            return "I am in Home";
+        }
+
+    }
+}
---
+++ Testing/xUnitExample/xunitExample.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using xUnitExample.Controllers;
+
+namespace xunitExample.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void HomeController_Index_ValidResult()
+        {
+            //AAA
+
+            //Arrange ->
+            HomeController controller = new HomeController();
+            string expectedResult = "I am in Home";
+
+            //Act ->
+            string res = controller.Index();
+
+            //Assert ->
+            res.Should().Be(expectedResult);
+
+        }
+
+    }
+}
